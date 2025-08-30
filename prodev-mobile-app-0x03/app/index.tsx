49d75eae7e925f3c182539b7a1113bbdd3c53179
Hanan Abdulshikur <hanabduman@gmail.com>
2025-08-30 07:31:07 +0300

    Update index.tsx

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,9 +1,7 @@
-import { styles } from "@/styles"
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native"
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
-
-import { FontAwesome, Ionicons } from "@expo/vector-icons"
-
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
   return (
@@ -13,40 +11,40 @@ export default function Index() {
           <Ionicons name="arrow-back" size={25} />
           <Image source={require('@/assets/images/logo.png')} />
         </View>
-
-        <Text style={styles.largeText}>Signin to yours</Text>
+        <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
         <Text style={styles.smallText}>
           Enter your email and password to sign in.
         </Text>
 
-        <View style={styles.formGroup} >
-
+        <View style={styles.formGroup}>
           <View>
             <Text style={styles.placeholderText}>Email</Text>
             <TextInput keyboardType="email-address" style={styles.inputField} />
           </View>
-
           <View style={{ marginTop: 20 }}>
             <Text style={styles.placeholderText}>Password</Text>
             <View style={styles.passwordGroup}>
               <TextInput style={{ flex: 1 }} />
-              <FontAwesome name="eye-slash" size={24} color="#7e7b7b" />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
             </View>
           </View>
-
           <Text style={styles.forgotPasswordText}>Forgot password?</Text>
         </View>
 
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>
-            Sign in
-          </Text>
+          <Text style={styles.buttonText}>Sign in</Text>
         </TouchableOpacity>
 
         <View style={styles.dividerGroup}>
-          <View style={styles.divider}></View>
-          <Text style={styles.dividerText}>Or</Text>
+          <View
+            style={styles.divider}
+          ></View>
+          <Text
+            style={styles.dividerText}
+          >
+            OR
+          </Text>
           <View
             style={styles.divider}
           ></View>
@@ -54,8 +52,14 @@ export default function Index() {
 
         <View style={styles.socialMediaButtonGroup}>
           <TouchableOpacity style={styles.socialMediaButton}>
-            <View style={{ flexDirection: 'row', alignItems: "center", gap: 5 }}>
-              <Image source={require("../assets/images/google.png")} />
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={require("@/assets/images/google.png")} />
               <Text style={styles.socialMediaButtonText}>
                 Continue with Google
               </Text>
@@ -63,7 +67,13 @@ export default function Index() {
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.socialMediaButton}>
-            <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
               <Image source={require("@/assets/images/facebook.png")} />
               <Text style={styles.socialMediaButtonText}>
                 Continue with Facebook
@@ -73,11 +83,10 @@ export default function Index() {
         </View>
 
         <View style={styles.subTextGroup}>
-          <Text style={styles.subText}>Dont't have an account</Text>
+          <Text style={styles.subText}>Don't have an account?</Text>
           <Text style={styles.subTextJoin}>Join now</Text>
         </View>
-        
       </SafeAreaView>
     </SafeAreaProvider>
-  )
+  );
 }
